d6cdf0ddaa84af016868f34bec2a6bf12b547fa7
saverioannpy <saverio.annunziata@outlook.com>
2023-11-14 17:07:47 +0100

    feat: funzione generatore numero avversario

--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -2,8 +2,13 @@
 
 /*** Funzioni ***/
 
+function cpuNumberGenerator(){
+    const cpuNumber = Math.floor(Math.random() * 4) + 1; //genero casualmente un numero tra 1 e 5
+    return cpuNumber;
+}
 
 
+/*** Main ***/
 
 let userChoice = 'pari'; //la variabile che conterrà la scelta viene inizializzata a "pari".
 let userNumber = 0;
@@ -20,15 +25,14 @@ do{
             if( ( userNumber >= 1 ) && ( userNumber <= 5 ) ){ //se il numero scelto è compreso fra 1 e 5 allora il gioco continua
                 alert("Hai scelto " + userNumber);
                 console.log("Scelta eseguita correttamente: " + userNumber);
-
-                //CONTINUARE IL GIOCO;
-
-
+                const cpuNumber = cpuNumberGenerator();//richiamo la funzione che genera un numero casuale per la cpu avversaria
+                alert("La CPU ha scelto " + cpuNumber);
+                console.log("La CPU ha scelto: " + cpuNumber);
             }else{
                 alert("Errore: Hai inserito un valore sbalgiato, scegli un numero compreso tra 1 e 5. Riprova.")
                 console.log("Errore: Hai inserito un valore sbalgiato, scegli un numero compreso tra 1 e 5. Riprova.")
             }
-        }while( ( userNumber < 1 ) || ( userNumber > 5 ) || ( isNaN(userNumber) ))
+        }while( ( userNumber < 1 ) || ( userNumber > 5 ) || ( isNaN(userNumber) )) //il programma chiederà all'utente di scegliere un numero fin quando esso non sarà tra 1 e 5
     }else{
         alert("Errore: Forse non ho capito bene la tua scelta, scegli tra pari o dispari. Riprova.")
         console.log("Errore: Forse non ho capito bene la tua scelta, scegli tra pari o dispari. Riprova.")
