e34426368a4a4aba6ad178fa0065a80ab91b1a89
saverioannpy <saverio.annunziata@outlook.com>
2023-11-14 16:01:25 +0100

    feat: eliminato array nella funzione ed aggiunta variabile vuota

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -3,23 +3,28 @@
 /******** Funzioni *******/
 
 function palindroma(parolaA){
-    const parolaB = []; //dichiaro un array in cui memorizzare l'input utente invertito
-    let i = parolaA.length-1; // il mio array avrà questo puntato inizializzato all'ultimo carattere della parola inserita dall'utente; 
-        for(let j = 0 ; j <= parolaA.length-1 ; j++){ // il  ciclo serve invece per scorre e riempire in maniera lineare l'array che conterrà la parola invertita
-            parolaB[j] = parolaA[i]; 
+    let parolaB = ''; 
+    let i = parolaA.length-1; // la mia funzione avrà due puntatori, i inizializzato all'ultimo carattere della parola inserita dall'utente e j inizializzato a 0 che incrementerà con un ciclo for
+        for(let j = 0 ; j <= parolaA.length-1 ; j++){ // il  ciclo serve invece per scorre e riempire in maniera lineare la variabile che conterrà la parola invertita
+            parolaB += parolaA[i]; //concateno al'interno della mia variabile ogni singolo carattere estrapolato a ritroso dalla parola inserita dall'utente
             console.log(parolaB[j]);
             i--; //inserisco l'ultimo carattere della parola data dall'utente alla posizione 0 del mio array e vado avanti così incrementando i e decrementando j
         }
-    console.log("La parola inserita dall'utente è: " + text);
-    console.log("La parola se viene invertita diventa:" + parolaB); 
-    return(parolaB);
+    let b = parolaB.toString();
+    console.log(b);
+    console.log("La parola inserita dall'utente è: " + parolaA);
+    console.log("La parola se viene invertita diventa: " + b); 
+    if(parolaA === parolaB){
+        alert("La parola inserita è palindroma");
+    }else{
+        alert("La parola inserita non è palindroma");
+    }
 }
 
 
 /******** Main *******/
 
 let parolaUtente = prompt("Inserisci una parola");
-console.log("Parola inserita dall'utente è: " + parolaUtente);
 palindroma(parolaUtente);
 
 
