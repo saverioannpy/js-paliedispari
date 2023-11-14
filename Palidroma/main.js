5ec86c98b47a937dd650cad846715f942b217942
saverioannpy <saverio.annunziata@outlook.com>
2023-11-14 15:03:58 +0100

    fix: rinomino variabili in maniera più chiara

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
-/******** Funzini *******/
-function palindroma(text){
+/******** Funzioni *******/
+
+function palindroma(parolaA){
     const parolaB = []; //dichiaro un array in cui memorizzare l'input utente invertito
-    let i = text.length-1; // il mio array avrà questo puntato inizializzato all'ultimo carattere della parola inserita dall'utente; 
-        for(let j = 0 ; j <= text.length-1 ; j++){ // il  ciclo serve invece per scorre e riempire in maniera lineare l'array che conterrà la parola invertita
-            parolaB[j] = text[i]; 
+    let i = parolaA.length-1; // il mio array avrà questo puntato inizializzato all'ultimo carattere della parola inserita dall'utente; 
+        for(let j = 0 ; j <= parolaA.length-1 ; j++){ // il  ciclo serve invece per scorre e riempire in maniera lineare l'array che conterrà la parola invertita
+            parolaB[j] = parolaA[i]; 
             console.log(parolaB[j]);
             i--; //inserisco l'ultimo carattere della parola data dall'utente alla posizione 0 del mio array e vado avanti così incrementando i e decrementando j
         }
@@ -17,8 +18,8 @@ function palindroma(text){
 
 /******** Main *******/
 
-let parolaA = prompt("Inserisci una parola");
-console.log("Parola inserita dall'utente è: " + parolaA);
-palindroma(parolaA);
+let parolaUtente = prompt("Inserisci una parola");
+console.log("Parola inserita dall'utente è: " + parolaUtente);
+palindroma(parolaUtente);
 
 
